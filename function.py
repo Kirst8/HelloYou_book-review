a06f36810be8aa3d710f38074c9a0ca6cd2c3a89
Kirst8 <kirst8.brooker@gmail.com>
2021-04-26 03:53:10 +0000

    isValid function created for form in function.py

---
+++ function.py
@@ -0,0 +1,18 @@
+import os
+wraps
+
+
+
+def isValid(email):
+    """Checks for valid email addresses
+    On the register form needs a genuine emails so isvalid to
+    validate emails address.
+    Args:
+        email: the email address supplied for validation.
+    Returns:
+        If email is address is valid it will return True if not then it is False.
+    """
+    if (re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email)
+       is not None):
+        return True
+    return False
